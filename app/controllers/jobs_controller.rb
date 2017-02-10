5f3415d5e9d134b971fe581e1400e94da045fa58
谢育初 <524945638@qq.com>
2017-02-10 22:57:03 +0800

    implement job CRUD

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,5 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
-
   def index
     @jobs = Job.all
   end
@@ -8,23 +7,22 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
-end
 
   def new
-    @job = Job.new
+  @job = Job.new
+end
+
+def create
+  @job = Job.new(job_params)
+
+  if @job.save
+    redirect_to jobs_path
+  else
+    render :new
   end
+end
 
-  def create
-    @job = Job.new(job_params)
-
-    if @job.save
-      redirect_to jobs_path
-    else
-      render :new
-    end
-  end
-
-  private
+private
 
   def job_params
     params.require(:job).permit(:title, :description)
@@ -43,7 +41,6 @@ end
     end
   end
 
-
   def destroy
     @job = Job.find(params[:id])
 
@@ -51,3 +48,5 @@ end
 
     redirect_to jobs_path
   end
+
+end
