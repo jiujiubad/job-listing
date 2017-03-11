cd41a75261e9c6437d4049139dd1c80309d64f63
谢育初 <524945638@qq.com>
2017-03-11 19:35:19 +0800

    step7

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,6 +1,9 @@
 class Job < ApplicationRecord
-  scope :published, -> { where(is_hidden: false) }
-  scope :recent, -> { order('created_at DESC') }
+  validates :title, presence: true
+  validates :wage_upper_bound, presence: true
+  validates :wage_lower_bound, presence: true
+  validates :wage_lower_bound, numericality: { greater_than: 0}
+  has_many :resumes
 
   def publish!
     self.is_hidden = false
@@ -11,8 +14,7 @@ class Job < ApplicationRecord
     self.is_hidden = true
     self.save
   end
-  validates :title, presence: true
-  validates :wage_upper_bound, presence: true
-  validates :wage_lower_bound, presence: true
-  validates :wage_lower_bound, numericality: { greater_than: 0}
+
+  scope :published, -> { where(is_hidden: false) }
+  scope :recent, -> { order('created_at DESC') }
 end
